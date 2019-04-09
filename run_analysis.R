4c70c9b42f88920193efab3d5ab454363b30a30b
stefanh30 <stefanh3008@gmail.com>
2019-04-09 09:45:10 +0200

    final script

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,4 @@
 #WK4 Assignment Coursera
-#Merges the training and the test sets to create one data set.
-#Extracts only the measurements on the mean and standard deviation for each measurement.
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive variable names.
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 #load data
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",stringsAsFactors=F)
@@ -11,26 +6,31 @@ features <- read.table("UCI HAR Dataset/features.txt",stringsAsFactors=F)
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt",stringsAsFactors=F)
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt",stringsAsFactors=F)
 y_train <- read.table("UCI HAR Dataset/train/Y_train.txt",stringsAsFactors=F)
-body_acc_x_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt",stringsAsFactors=F)
-body_acc_y_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt",stringsAsFactors=F)
-body_acc_z_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt",stringsAsFactors=F)
-body_gyro_x_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt",stringsAsFactors=F)
-body_gyro_y_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt",stringsAsFactors=F)
-body_gyro_z_train <- read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt",stringsAsFactors=F)
-total_acc_x_train <- read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt",stringsAsFactors=F)
-total_acc_y_train <- read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt",stringsAsFactors=F)
-total_acc_z_train <- read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt",stringsAsFactors=F)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt",stringsAsFactors=F)
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt",stringsAsFactors=F)
+y_test <- read.table("UCI HAR Dataset/test/Y_test.txt",stringsAsFactors=F)
 
 #label data
 colnames(x_train) <- features$V2
+colnames(x_test) <- features$V2
 library(plyr)
 y_train <- join(y_train, activity_labels, by = "V1")
+y_test <- join(y_test, activity_labels, by = "V1")
 rm(activity_labels,features)
 
-#Join tables
-df<-cbind(y_train$V2,subject_train,x_train,body_acc_x_train,body_acc_y_train,body_acc_z_train,body_gyro_x_train, 
-          body_gyro_y_train, body_gyro_z_train, total_acc_x_train, total_acc_y_train, total_acc_z_train)
-rm(y_train,subject_train,x_train,body_acc_x_train,body_acc_y_train,body_acc_z_train,body_gyro_x_train, 
-   body_gyro_y_train, body_gyro_z_train, total_acc_x_train, total_acc_y_train, total_acc_z_train)
+#Merge into one data frame
+df<-cbind(y_train$V2,subject_train,x_train)
+df2<-cbind(y_test$V2,subject_test,x_test)
+colnames(df)[1] <- "activity"
+colnames(df2)[1] <- "activity"
+df<-rbind(df,df2)
+rm(y_train,subject_train,x_train,df2,x_test,y_test,subject_test)
+colnames(df)[2] <- "subject"
 
-colnames(df)
+#Extracts metrics mean and standard deviation
+df <- df[,c(1,2,grep("mean\\(|std\\(",colnames(df)))]
+
+#creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(dplyr)
+dfsum<-(df %>% group_by(activity,subject) %>%  summarise_all(funs(mean),na.rm=T))
+
